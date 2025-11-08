7d4467d158fb3f19b2015b2ee608b169f672959c
Nam Anh Tran <steve200994@gmail.com>
2025-11-09 10:08:40 +1100

    try again

--- frontend/ecom/src/main.ts
+++ frontend/ecom/src/main.ts
@@ -3,20 +3,10 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import 'preline/preline';
 
-// Wait until Tailwind CSS finishes loading before starting Angular
-async function waitForTailwind(): Promise<void> {
-  return new Promise((resolve) => {
-    if ((window as any).tailwindReady) return resolve();
-    const check = setInterval(() => {
-      if ((window as any).tailwindReady) {
-        clearInterval(check);
-        resolve();
-      }
-    }, 10); // check every 10ms
-  });
-}
 
-waitForTailwind()
-  .then(() => bootstrapApplication(App, appConfig))
-  .then(() => window.dispatchEvent(new Event('AngularReady')))
+bootstrapApplication(App, appConfig)
+  .then(() => {
+    window.dispatchEvent(new Event('AngularReady'));
+  })
   .catch((err) => console.error(err));
+
